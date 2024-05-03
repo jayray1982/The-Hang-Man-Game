7c2f3e8aca361c79a3d6a5068a7283f3c23431db
DJJay <DJJay@DJJAYITO-ASUSPC>
2024-05-03 02:29:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Hang_Man_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
