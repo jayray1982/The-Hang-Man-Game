263eedff0c431686c6376ff3edbb4fd436bab9bc
James Miller-Suttmeier <Jayray22@gmail.com>
2024-07-11 15:17:40 -0400

    Updated how the guesssword length is figured out.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var mystery_words = new List<string> (){ "Apple", "Bat", "Cat", "Dog", "Empty", "Flowers", "Gummy", "Hospital", "Ice Coffee", "Joy Stick" };
+            Console.WriteLine("list count is " +mystery_words.Count+ ".");
+            Console.WriteLine(mystery_words[6]);
         }
     }
 }
